dbc1f9a598e1ae377d4f837bb5f12685d3f00ab9
nishant niket <nishantniket@gmail.com>
2019-12-10 21:00:15 +0530

    Add project files.

---
+++ AzureAdb2cSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAdb2cSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
